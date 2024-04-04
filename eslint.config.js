1df86e45693f66875f3b9ff0a0ced64d68c73501
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-04-03 23:25:21 -0400

    reinstall prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,28 @@
+import prettier from "eslint-config-prettier";
 import config from "@antfu/eslint-config";
 
 export default config({
 	rules: {
-		"curly": "off",
+		curly: "off",
 		"no-console": "off",
 		"jsonc/sort-keys": "off",
 		"ts/ban-ts-comment": "off",
 		"ts/prefer-ts-expect-error": "off",
+		"import/order": [
+			"error",
+			{
+				pathGroupsExcludedImportTypes: ["builtin"],
+				pathGroups: [
+					{
+						pattern: "@**/**",
+						group: "external",
+						position: "after",
+					},
+				],
+			},
+		],
 	},
-	stylistic: {
-		indent: "tab",
-		quotes: "double",
-		semi: true,
-		overrides: {
-			"antfu/if-newline": "off",
-			"style/max-len": ["error", { code: 100 }],
-		},
-	},
+	stylistic: false,
 	jsx: false,
 	toml: false,
-});
+}).append(prettier);
